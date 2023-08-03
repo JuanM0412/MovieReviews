12998c41a5836b631eb7f50e8162f689e73a7f5c
JuanM0412 <juangomezp1204@gmail.com>
2023-08-03 06:12:54 -0500

    Página about y cambio de color en la página home

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Juan Manuel Gómez'})
+
+
+def about(request):
+    return render(request, 'about.html')
