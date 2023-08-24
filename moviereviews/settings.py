493468d41a1c9b543926fdabe2a81d1e71e23008
JuanM0412 <juangomezp1204@gmail.com>
2023-08-24 06:08:24 -0500

    Taller 2 terminado Modelo news añadido. También se creó el navbar.

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movie'
+    'movie',
+    'news'
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviereviews/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +71,13 @@ TEMPLATES = [
         },
     },
 ]
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    'moviereviews/templates'
+]
+
 
 WSGI_APPLICATION = 'moviereviews.wsgi.application'
 
